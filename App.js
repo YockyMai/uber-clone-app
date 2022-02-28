d71a35f3cb7ca1958a9b53f35b9ba5f880d9cc01
Grigoriev Valerii <g.valera15102003@gmail.com>
2022-02-28 20:12:14 +0500

    init application

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import {Provider} from "react-redux";
+import {createStore} from "@reduxjs/toolkit";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <Provider store={store}>
+            <View style={styles.container}>
+                <Text>Данил лqох</Text>
+                <StatusBar style="auto"/>
+            </View>
+        </Provider>
+    );
 }
 
+const store = createStore({
+})
+
+createStore(store);
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 });
